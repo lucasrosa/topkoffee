1b24f34de86ad7a9c9a025bf6a4762794933e78f
Lucas Furlani rosa <lucas.furlani.rosa@gmail.com>
2018-09-16 21:58:33 -0300

    More cleanup to javascript

--- coffee/src/index.js
+++ coffee/src/index.js
@@ -1,7 +1,4 @@
 import $ from "jquery";
-// import './superfish.min';
-// import './jquery.magnific-popup.min';
-//import './waypoints.min';
 
 $(document).ready(function(){
 	"use strict";
@@ -9,43 +6,11 @@ $(document).ready(function(){
 	var window_width 	 = $(window).width(),
 	window_height 		 = window.innerHeight,
 	header_height 		 = $(".default-header").height(),
-	header_height_static = $(".site-header.static").outerHeight(),
 	fitscreen 			 = window_height - header_height;
-
 
 	$(".fullscreen").css("height", window_height)
 	$(".fitscreen").css("height", fitscreen);
-
-  
-    $(document).ready(function() {
-
-    $('html, body').hide();
-
-        if (window.location.hash) {
-
-        setTimeout(function() {
-
-        $('html, body').scrollTop(0).show();
-
-        $('html, body').animate({
-
-        scrollTop: $(window.location.hash).offset().top
-
-        }, 1000)
-
-        }, 0);
-
-        }
-
-        else {
-
-        $('html, body').show();
-
-        }
-
-    });
-  
-
+ 
   // Header scroll class
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
