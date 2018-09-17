e30db031366c2d302377a147e49fa7971c052eeb
Lucas Furlani rosa <lucas.furlani.rosa@gmail.com>
2018-09-17 20:38:07 -0300

    Fixed webpack to use jsx and started cleaning up jquery dependency

---
+++ coffee/src/components/RankBox.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class RankBox extends Component {
+    render() {
+        return (
+            <div>
+            Hello bello {this.props.name}
+            </div>
+        );
+    }
+}
+
